cad5efa232a94486bcb7217ec84f3078dfcc7331
Swapnapxy <146064964+Swapnapxy@users.noreply.github.com>
2023-09-27 16:55:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+ 
+function change(){
+        
+var a="Flower-Name:";
+    if(true){
+   document.getElementById("var").textContent=a;
+   a="Rose";
+        
+    document.getElementById("var1").textContent=a; 
+        a="Rose is beautiful flower "
+    }
+    document.getElementById("var5").textContent=a;
+}
+    
+function change1(){
+    let b="Animal-name:";
+    if(true){
+        document.getElementById("var2").textContent=b;
+        b="Rabbit"
+        document.getElementById("var3").textContent=b;
+        
+    }
+}
+function change2(){
+    const c="Butterfly";
+    document.getElementById("var4").textContent=c;
+}
